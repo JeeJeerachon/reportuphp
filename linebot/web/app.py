1fa5bef8469ca1fc922448c42f77c7e63544e406
teerat1 <noob444555@gmail.com>
2021-07-03 14:34:35 +0700

    test

--- linebot/web/app.py
+++ linebot/web/app.py
@@ -3,13 +3,13 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-# app.config['MYSQL_USER']='root'
-# app.config['MYSQL_PASSWORD']=''
-# app.config['MYSQL_HOST']='localhost'
-# app.config['MYSQL_DB']=''
+ app.config['MYSQL_USER']='root'
+ app.config['MYSQL_PASSWORD']=''
+ app.config['MYSQL_HOST']='localhost'
+ app.config['MYSQL_DB']='sqltest1'
 # app.config['MYSQL_CURSORCLASS']='DictCursor'
 
-# mysql = MySQL(app)
+mysql = MySQL(app)
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
@@ -22,15 +22,15 @@ def addToken():
         password =request.form['pass']
         token = request.form['token_line']
 
-    print(username)
-    print(password)
-    print(token)
+  #  print(username)
+   # print(password)
+    #print(token)
 
     return "ควย"
 
-# cur = mysql.connection.cursor()
-# cur.execute("INSERT INTO member (name,username,password) VALUES (%s,%s,%s)",(name,username,password)) 
-# mysql.connection.commit() 
+ cur = mysql.connection.cursor()
+ cur.execute("INSERT INTO line (Username,token) VALUES (%s,%s,%s)",(username,token)) 
+ mysql.connection.commit() 
 
 
 if __name__=="__main__":
